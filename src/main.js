ea9451240d10cbead4d29a887ad7356dbcfd8ec7
let_free <2634044588@qq.com>
2025-03-11 15:51:11 +0800

    修改高危漏洞

--- src/main.js
+++ src/main.js
@@ -8,30 +8,23 @@ Vue.config.productionTip = false
 
 import Element from 'element-ui'
 import "element-ui/lib/theme-chalk/index.css"
+
 Vue.use(Element)
 
 import axios from 'axios'
 import "./axios.js"
 //引用全局
-Vue.prototype.$axios = axios 
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
-
-import * as echarts from 'echarts';
-Vue.prototype.$echarts = echarts;
 // 过滤器
 
 import 'ol/ol.css'
 import "./mapOperation.js"
 
-//引入Antd
-// import "ant-design-vue/dist/antd.less";
 new Vue({
-  router,
-  store,
-  data: {
-    // 空的实例放到根组件下，所有的子组件都能调用
-    Bus: new Vue()
-  },
-  render: h => h(App)
-  
+    router, store, data: {
+        // 空的实例放到根组件下，所有的子组件都能调用
+        Bus: new Vue()
+    }, render: h => h(App)
+
 }).$mount('#app')
