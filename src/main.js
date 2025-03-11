176c55c97cca23cc3f10ce5d0f3bbc7dbe4cc842
let_free <2634044588@qq.com>
2025-03-11 15:47:35 +0800

    修改高危漏洞

--- src/main.js
+++ src/main.js
@@ -16,26 +16,13 @@ import "./axios.js"
 Vue.prototype.$axios = axios 
 Vue.config.productionTip = false
 
-// 引入echarts
-// import echarts from 'echarts'
-
-// import echarts from 'echarts';
 import * as echarts from 'echarts';
 Vue.prototype.$echarts = echarts;
 // 过滤器
-import * as custom from './utils/util'
-import ol from 'ol'
+
 import 'ol/ol.css'
 import "./mapOperation.js"
-// import Tiff from 'tiff.js'
-import Popup from 'ol-popup'
-// import on your project (less then 1KB gziped)
-// import vueSmoothScroll from 'vue2-smooth-scroll'
-// Vue.use(vueSmoothScroll)
-import shp from 'shapefile';
-import togeojson from 'togeojson';
-import smooth from 'chaikin-smooth'
-import moment from 'moment'
+
 //引入Antd
 // import "ant-design-vue/dist/antd.less";
 new Vue({
